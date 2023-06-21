fcc772fe616b2719758573c75cb26291163f010c
rakuszz0 <jiwagila023@gmail.com>
2023-06-21 09:20:22 +0700

    d

--- routes/contact.go
+++ routes/contact.go
@@ -12,7 +12,7 @@ func ContactRoutes(e *echo.Group) {
 	contactRepository := repositories.RepositoryContact(mysql.DB)
 	h := handlers.HandlerContact(contactRepository)
 
-	e.GET("/contacts", h.FindContacts)
+	e.GET("/contacts", h.GetContactList)
 	e.GET("/contact/:id", h.GetContact)
 	e.POST("/contact", h.CreateContact)
 	e.PATCH("/contact/:id", h.UpdateContact)
